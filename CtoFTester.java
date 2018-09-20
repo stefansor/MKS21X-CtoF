0d57620f997bd5def7f288ba45f944519e578bc2
stefansor <ssorobay00@stuy.edu>
2018-09-20 19:52:29 -0400

    fixed hopefully

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,12 +1,12 @@
 public class CtoFTester {
-  public static void main(String[] args ) {
-    public int celsiusToFahrenheit(int a){
-      return (((a * 9)/5) + 32);
-    }
+  public static int celsiusToFahrenheit(int a){
+    return (((a * 9)/5) + 32);
+  }
 
-    public int fahrenheitToCelsius(int b){
-      return (((a - 32) * 5) / 9);
-    }
+  public static int fahrenheitToCelsius(int b){
+    return (((b - 32) * 5) / 9);
+  }
+  public static void main(String[] args ) {
     if (celsiusToFahrenheit(-10) == 14){
       if (fahrenheitToCelsius(14) == -10){
         System.out.println("-10 degrees celsius is equal to 14 degrees fahrenheit");
